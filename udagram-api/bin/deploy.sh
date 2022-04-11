d5ed360ee385588a1cddffead33e9cb1b2a60656
Yousef Adel <yousefjooo2018@gmail.com>
2022-04-11 02:56:30 +0200

    first commit

---
+++ udagram-api/bin/deploy.sh
@@ -0,0 +1,6 @@
+eb init udagramV2 --platform node.js --region us-east-1 &&
+eb use Udagramv2-env && eb setenv POSTGRES_USERNAME=$POSTGRES_USERNAME POSTGRES_PASSWORD=$POSTGRES_PASSWORD POSTGRES_DB=$POSTGRES_DB DB_PORT=$DB_PORT POSTGRES_HOST=$POSTGRES_HOST JWT_SECRET=$JWT_SECRET &&
+eb deploy Udagramv2-env
+
+
+ 
