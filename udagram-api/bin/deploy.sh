31f5f4dc866b775f1de6b58b022e20eaceb7911b
Yousef Adel <yousefjooo2018@gmail.com>
2022-04-12 07:47:55 +0200

    v7

--- udagram-api/bin/deploy.sh
+++ udagram-api/bin/deploy.sh
@@ -1,4 +1,4 @@
-eb init udagramV2 --platform node.js-16 --region us-east-1 &&
+eb init udagramV2 --platform "Node.js 16 running on 64bit Amazon Linux 2" --region us-east-1 &&
 eb use Udagramv2-env && eb setenv POSTGRES_USERNAME=$POSTGRES_USERNAME POSTGRES_PASSWORD=$POSTGRES_PASSWORD POSTGRES_DB=$POSTGRES_DB DB_PORT=$DB_PORT POSTGRES_HOST=$POSTGRES_HOST JWT_SECRET=$JWT_SECRET &&
 eb deploy Udagramv2-env
 
